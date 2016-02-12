ee9c1333f232064fe37df9bea7f61f41a9fa2e15
msirbu-umd <msirbu-498@users.noreply.github.com>
2016-02-12 10:58:33 -0500

    Let the button now send the threshold to R, also saves the threshold for later use.

--- src/epiviz/data/request.js
+++ src/epiviz/data/request.js
@@ -358,11 +358,15 @@ epiviz.data.Request.updateWidthRequest = function(e){
   console.log(e.args);
   console.log(e.args["min"]);
   console.log(e.args["max"]);*/
+  //console.log("THE REQUEST IS");
+  //console.log(e);
   return epiviz.data.Request.createRequest({
     version: epiviz.EpiViz.VERSION,
     action: epiviz.data.Request.Action.UPDATE_WIDTH,
     chartId: e.id,
+    datasource: e.args["datasource"],
     min: e.args["min"],
-    max: e.args["max"]
+    max: e.args["max"],
+    threshold: e.args["threshold"]
   });
 };
--- src/epiviz/ui/charts/decoration/update-width-button.js
+++ src/epiviz/ui/charts/decoration/update-width-button.js
@@ -11,6 +11,8 @@ goog.provide('epiviz.ui.charts.decoration.UpdateWidthButton');
  */
 epiviz.ui.charts.decoration.UpdateWidthButton = function(visualization, otherDecoration) {
     epiviz.ui.charts.decoration.ChartOptionButton.call(this, visualization, otherDecoration);
+
+    this._threshold = 0;
 };
 
 /*
@@ -24,13 +26,53 @@ epiviz.ui.charts.decoration.UpdateWidthButton.constructor = epiviz.ui.charts.dec
  * @protected
  */
 epiviz.ui.charts.decoration.UpdateWidthButton.prototype._click = function() {
-    var self = this;
+
+   /* var self = this;
     return function(){
         //THIS NEEDS TO BE INSIDE THE FUNCTION
-        console.log("Fire the update width!");
+        alert("AH YEAH!");
+        alert("HEHEHEHE")
+        //console.log("Fire the update width!");
+        //alert(self.visualization().measurements().first().datasource().id());
         self.visualization().onUpdateWidth().notify(new epiviz.ui.charts.VisEventArgs(self.visualization().id(),
-            {min: 5, max: 10}));
+            {min: 5, max: 20, datasource: self.visualization().measurements().first().datasource().id()})); //datasource: self.visualization().measurements().first().datasource().id()}));
         //5, max: 10)); self.visualization().id()));
+    };*/
+
+    var self = this;
+    console.log("ME SELF");
+    console.log(self);
+    console.log(self._threshold);
+    return function(){
+
+        console.log("HAAHAHAHAHAHAHA");
+        console.log(self.visualization().properties().customSettingsDefs);
+        console.log("--------------");
+        console.log(self.visualization().customSettingsValues());
+        console.log("2222-------------------2222");
+        console.log(self._threshold);
+
+        var UpdateWidthDialog = new epiviz.ui.controls.UpdateWidthDialog(
+            'Threshold Menu!', {
+                ok: function(updateWidthValues) {
+                    alert(updateWidthValues);
+                    alert(updateWidthValues['threshold']);
+                    //TODO: Copy the notify on line 35 here and change min and max to threshold
+                    //TODO: Also change epivizr to accept threshold
+                    self._threshold = updateWidthValues['threshold'];
+                    self.visualization().onUpdateWidth().notify(new epiviz.ui.charts.VisEventArgs(self.visualization().id(),
+                        {min: 5, max: 20, threshold: updateWidthValues['threshold'], datasource: self.visualization().measurements().first().datasource().id()}));
+                    //console.log(CustomSettings);
+                    //console.log(self.visualization().properties().customSettingsDefs)
+                    //console.log(self.visualization().customSettingsValues());
+                    //self.visualization().setCustomSettingsValues(settingsValues);
+                },
+                cancel: function() {}
+            },
+            //self.visualization().properties().customSettingsDefs,
+            [new epiviz.ui.charts.CustomSetting("threshold", "number", 500, "min threshold", null)],
+            {"threshold": self._threshold});
+        UpdateWidthDialog.show();
     };
 
 };
